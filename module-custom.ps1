146e95127815a2792d3d8ea628a5be3de3a335ec
SimoGHcoder <delu.simo@ittmarconiforli.edu.it>
2026-08-17 09:26:12 +0200

    Exclude .gitkeep files in item list
    Added filter to exclude .gitkeep files from the list.

--- module-custom.ps1
+++ module-custom.ps1
@@ -4,7 +4,8 @@ function global:Get-ModuleCustomList {
     try {
         $items = Invoke-RestMethod -Uri $url -UseBasicParsing -ErrorAction Stop
         foreach ($item in $items) {
-            if ($item.type -eq "file") {
+            # Filtro: controlla che sia un file e che NON sia .gitkeep
+            if ($item.type -eq "file" -and $item.name -ne ".gitkeep") {
                 $list += [PSCustomObject]@{
                     Name        = $item.name
                     DownloadUrl = $item.download_url
@@ -12,7 +13,7 @@ function global:Get-ModuleCustomList {
             }
         }
     } catch {
-        # Fallback se la cartella installer/ non esiste o è vuota
+        # Cartella vuota o non presente
     }
     return $list
 }
